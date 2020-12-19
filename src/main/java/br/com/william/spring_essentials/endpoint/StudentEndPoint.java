dabb8ed110b10bd1eacf809eedd0a8096bd7b55f
John William Vicente <johnwill.v.2017@gmail.com>
2020-12-19 16:41:54 -0300

    Padrões REST e POST pt 01

--- src/main/java/br/com/william/spring_essentials/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/william/spring_essentials/endpoint/StudentEndPoint.java
@@ -4,8 +4,13 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +20,7 @@ import br.com.william.spring_essentials.model.Student;
 import br.com.william.spring_essentials.util.DateUtil;
 
 @RestController // @Controller- controler. @ResponseBody- retornar dados pelo corpo da resposta
-@RequestMapping("/v1/student")
+@RequestMapping("/v1/students")
 public class StudentEndPoint {
 
 	private DateUtil dateUtil;
@@ -29,11 +34,21 @@ public class StudentEndPoint {
 
 
 
-	@RequestMapping(path = "/list", method = RequestMethod.GET)
-	private List<?> getListStudent() {
+	@RequestMapping(method = RequestMethod.GET)
+	private ResponseEntity<?> getListStudent() {
 		System.out.println(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-		return Arrays.asList(new Student("John William"), new Student("John Wick"), new Student("William Suane"),
-				new Student("Brennon"));
+		return new ResponseEntity<>(Student.getStudentList(), HttpStatus.OK);
+
+	}
+	@RequestMapping(path ="/{id}" ,method = RequestMethod.GET)
+	private ResponseEntity<?> getStudentByID(@PathVariable("id")int id) {
+		Student student = new Student();
+		student.setId(id);
+		int index = Student.getStudentList().indexOf(student);
+		
+		if(index>=0) student = Student.getStudentList().get(index);
+		else student = null;
+		return new ResponseEntity<>(student, HttpStatus.OK);
 
 	}
 }
--- src/main/java/br/com/william/spring_essentials/model/Student.java
+++ src/main/java/br/com/william/spring_essentials/model/Student.java
@@ -1,8 +1,19 @@
 package br.com.william.spring_essentials.model;
 
-public class Student {
-	private String name;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Student {
+	private int id;
+	private String name;
+	private static List<Student> studentList;
+	static {
+		studentList = new ArrayList<Student>(
+				Arrays.asList(new Student(0,"John William"), new Student(1,"John Wick"), new Student(2,"William Suane"),
+						new Student(3,"Brennon"))
+				);
+	}
 	public Student(String name) {
 		super();
 		this.name = name;
@@ -12,6 +23,11 @@ public class Student {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Student(int id, String name) {
+		this(name);
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
@@ -20,5 +36,45 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public static List<Student> getStudentList() {
+		return studentList;
+	}
+
+	public static void setStudentList(List<Student> studentList) {
+		Student.studentList = studentList;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
 	
 }
